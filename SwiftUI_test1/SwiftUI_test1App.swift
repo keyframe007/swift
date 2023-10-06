0ba48702bec77d92e92abc23f96a3bd4d17b995f
keyframe007 <keyframe007@gmail.com>
2023-10-06 18:21:56 -0400

    Initial Commit

---
+++ SwiftUI_test1/SwiftUI_test1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_test1App.swift
+//  SwiftUI_test1
+//
+//  Created by Ali Tezel on 2023-10-06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
