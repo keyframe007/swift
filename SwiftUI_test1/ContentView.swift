1ec0e1631e2f8c94997e7a9ba4d82e6975eb1b11
keyframe007 <keyframe007@gmail.com>
2023-10-06 19:14:52 -0400

    added cloud image and temperature text

--- SwiftUI_test1/ContentView.swift
+++ SwiftUI_test1/ContentView.swift
@@ -11,22 +11,34 @@ struct ContentView: View {
     var body: some View {
         ZStack{
             LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: /*@START_MENU_TOKEN@*/.topLeading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.bottomTrailing/*@END_MENU_TOKEN@*/)
+                .ignoresSafeArea()
+            
             VStack {
-                Text("Weather")
+                Text("Montreal, QC")
+                    .font(.system(size: 30,
+                                  weight: .bold,
+                                  design: .rounded))
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.white)
+                Image(systemName: "cloud.bolt.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180.0,height: 180.0)
+                
+                Text("24°")
                     .font(.system(size: 40,
                                   weight: .bold,
                                   design: .rounded))
-                    .foregroundStyle(.white)
-                Text("Some text")
-                    .font(.system(size: 30,
-                                  weight: .semibold))
-                    .foregroundStyle(.blue)
+                    .foregroundColor(.white)
+                    
                 Spacer()
                 
                 
                 
             }
-            .padding()
+            .padding(.top, 30.0)
         }
     }
 }
