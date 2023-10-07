cfee37619cc8a474ec2ba4b17c62d3a1f238d9c1
keyframe007 <keyframe007@gmail.com>
2023-10-06 20:57:18 -0400

    added small elements to the bottom

--- SwiftUI_test1/ContentView.swift
+++ SwiftUI_test1/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
             LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: /*@START_MENU_TOKEN@*/.topLeading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.bottomTrailing/*@END_MENU_TOKEN@*/)
                 .ignoresSafeArea()
             
+            //TOP Element with the city name, cloud and temperature
             VStack {
                 Text("Montreal, QC")
                     .font(.system(size: 30,
@@ -20,11 +21,13 @@ struct ContentView: View {
                                   design: .rounded))
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
+                    .padding(.top, 12.0)
                     .foregroundStyle(.white)
-                Image(systemName: "cloud.bolt.fill")
+                Image(systemName: "cloud.sun.fill")
                     .renderingMode(.original)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
+                    .padding(.top, -40.0)
                     .frame(width: 180.0,height: 180.0)
                 
                 Text("24°")
@@ -32,16 +35,45 @@ struct ContentView: View {
                                   weight: .bold,
                                   design: .rounded))
                     .foregroundColor(.white)
-                    
+                    .padding(.top, -50.0)
+                
                 Spacer()
-                
-                
-                
+             //END top element
             }
-            .padding(.top, 30.0)
+            
+            HStack {
+                
+                VStack{
+                    Text("MON")
+                        .font(.system(size: 16,
+                                      weight: .bold,
+                                      design: .rounded))
+                        .foregroundStyle(.white)
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 60, height: 60)
+                    Text("22°")
+                        .font(.system(size: 24,
+                                      weight: .bold,
+                                      design: .rounded))
+                        .foregroundColor(.white)
+                    
+                    
+                    
+                    
+                    
+                    
+                }
+            }
         }
     }
 }
 #Preview {
     ContentView()
 }
+            
+            
+                
+                
